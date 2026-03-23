5b34a32d47e4be3cd4c5a63d753cbc85bca59ccd
aolanioaks <aolanioaks@gmail.com>
2026-03-23 14:51:34 -0600

    jljlk

--- public/app.js
+++ public/app.js
@@ -357,7 +357,7 @@ const app = createApp({
 
     formatDate(dateStr) {
       if(!dateStr) return "";
-      return new Date(dateStr).toLocaleDateString({ month: "short", day: "numeric", year: "numeric" });
+      return new Date(dateStr).toLocaleDateString('en-US',{ month: "short", day: "numeric", year: "numeric" });
     },
 
 
