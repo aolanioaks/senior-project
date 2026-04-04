34f4a01addf0766bc47bf48a65cf734c6132b525
aolanioaks <aolanioaks@gmail.com>
2026-04-04 14:28:48 -0600

    working on it

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const uploadsFolder = path.join(__dirname, "uploads");
 
